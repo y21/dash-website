180c92e8624129833bf3bc95fef1648c1e6f4393
y21 <30553356+y21@users.noreply.github.com>
2022-11-06 15:52:22 +0100

    rewrite

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,187 +1,172 @@
 import React, { Suspense, Component } from 'react';
+import dash, { ExternalVm, JsValue, OptLevel, infer, debug } from './pkg/wasm';
 import './Playground.css';
-import * as dash from './vendor/dash';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import { getRevision } from './util';
+import { Emit } from './pkg/wasm';
 
 const Editor = React.lazy(() => import('@monaco-editor/react'));
 const Empty = <></>;
 
-// TODO(y21): typings
-declare var EmbeddedConsole: any;
-
-const DEFAULT_CODE = `
-// JavaScript code goes here
-function* fib() {
-    let a = 1;
-    let b = 1;
-    let current = 1;
-  
-    yield current;
-  
-    while (true) {
-        current = b;
-  
-        yield current;
-  
-        b = a + b;
-        a = current;
-    }
+const enum Mode {
+    EVAL,
+    TYPES,
+    BYTECODE,
+    JS
 }
 
-const values = [];
-const seq = fib();
-
-for (let i = 0; i < 20; ++i) values.push(seq.next().value + 0);
-
-values
-`.trim();
-
 interface PlaygroundState {
-    console: any;
-    editor: monaco.editor.IStandaloneCodeEditor,
-    engine?: dash.Engine,
-    vm?: dash.VM
+    editor: monaco.editor.IStandaloneCodeEditor | null,
+    vm: ExternalVm | null,
+    output: string,
 }
 
 class Playground extends Component<{}, PlaygroundState> {
+    autorun = true;
+    mode = Mode.EVAL;
+
+    constructor(props: {}) {
+        super(props);
+        this.state = { editor: null, vm: null, output: '' };
+    }
+
     async componentDidMount() {
-        const consoleElement = document.getElementById('console');
-        if (!consoleElement) return this.reportError('console not found');
+    }
 
-        const ec = new EmbeddedConsole(consoleElement, {
-            height: '300px'
-        });
+    dashEval(vm: ExternalVm, code: string): string {
+        const value = vm.eval(code, OptLevel.Aggressive);
+        const output = value.to_js_string(vm);
 
-        this.setState({
-            ...this.state,
-            console: ec
-        });
+        return output;
+    }
 
-        ec.log("Welcome to the dash playground");
-        ec.log("Type any JavaScript code in the editor above and press \"Run\"");
-        ec.log("The last expression result will appear in this console");
+    dashInfer(code: string): string {
+        return infer(code);
+    }
 
-        const rev = await getRevision();
-        ec.log(`📦 Build: ${rev.substr(0, 7)}`)
+    dashBytecode(code: string): string {
+        return debug(code, OptLevel.Aggressive, Emit.Bytecode);
+    }
+
+    dashDesugarJs(code: string): string {
+        return debug(code, OptLevel.Aggressive, Emit.JavaScript);
     }
 
     async onCodeRun() {
+        console.log(this.mode)
         const code = this.getCode();
         if (!code) return this.reportError('failed to get code');
 
         localStorage.setItem('code', code);
 
         const vm = await this.getOrInitVM();
-
         try {
-            let result = vm.eval(code);
+            let output;
+            switch (this.mode) {
+                case Mode.EVAL:
+                    output = this.dashEval(vm, code);
+                    break;
+                case Mode.TYPES:
+                    output = this.dashInfer(code);
+                    break;
+                case Mode.BYTECODE:
+                    output = this.dashBytecode(code);
+                    break;
+                case Mode.JS:
+                    output = this.dashDesugarJs(code);
+                    break;
+                default:
+                    throw new Error('Unsupported mode');
+            }
 
-            // Try to convert the result to a number
-            const maybeNumber = Number(result);
-
-            // ... and print it if it is a number
-            // we do this because numbers have special higlighting in the console
-            // later once the WASM API is more mature and exposes values directly,
-            // we can do better than that
-            if (!isNaN(maybeNumber)) this.state.console.log(maybeNumber);
-            else this.state.console.log(result);
+            this.setState({ ...this.state, output });
         } catch (e) {
-            this.reportError(e);
+            console.log(e);
+            if (e instanceof JsValue) {
+                this.setState({ ...this.state, output: e.to_js_string(vm) });
+            } else if (typeof e === 'string') {
+                this.setState({ ...this.state, output: e });
+            } else {
+                this.setState({ ...this.state, output: '<Unknown error>' });
+            }
         }
-    }
-
-    onCodeSave() {
-        const code = this.getCode();
-        if (!code) return this.reportError('failed to get code');
-
-        // Base64 encode code 
-        const encoded = btoa(code);
-        document.location.href += `?code=${encoded}`;
     }
 
     getCode() {
-        return this.state.editor.getModel()?.getValue();
+        return this.state.editor?.getModel()?.getValue();
     }
 
     async getOrInitVM() {
-        const engine = this.state.engine || new dash.Engine();
-
-        if (!this.state.engine) {
-            this.setState({ ...this.state, engine });
-        }
-
-        if (!engine.initialized) {
-            await engine.init('./assets/dash.wasm');
-        }
-
-        const vm = this.state.vm || engine.createVM();
-
-        if (!this.state.vm) {
+        let vm = this.state.vm;
+        if (!vm) {
+            await dash();
+            vm = new ExternalVm();
             this.setState({ ...this.state, vm });
         }
-
         return vm;
     }
 
     editorDidMount(editor: monaco.editor.IStandaloneCodeEditor) {
-        const model = editor.getModel();
-        if (!model) return this.reportError('failed to get monaco editor model');
-
-        const params = new URLSearchParams(document.location.search);
-        const code = params.get('code');
-
-        if (code) {
-            model.setValue(atob(code));
-        } else {
-            const code = localStorage.getItem('code') || DEFAULT_CODE;
-            model.setValue(code.trim());
-        }
-
         this.setState({ ...this.state, editor });
     }
 
     reportError(...args: any[]) {
-        const error = this.state.console.error.bind(this.state.console) || console.error;
-        error(...args);
+        // const error = this.state.console.error.bind(this.state.console) || console.error;
+        // error(...args);
+    }
+
+    onType() {
+        if (this.autorun) {
+            this.onCodeRun();
+        }
+    }
+
+    toggleAutorun() {
+        this.autorun = !this.autorun;
+    }
+
+    setMode() {
+        const select = document.getElementById('mode') as HTMLSelectElement;
+        switch (select.value) {
+            case 'eval': this.mode = Mode.EVAL; break;
+            case 'types': this.mode = Mode.TYPES; break;
+            case 'bytecode': this.mode = Mode.BYTECODE; break;
+            case 'js': this.mode = Mode.JS; break;
+        }
     }
 
     render() {
         return (
-            <div className="App center">
-                <div id="buttons">
-                    <span className="button" onClick={this.onCodeRun.bind(this)}>
-                        <img src="./assets/playicon.svg" width="40" className="purple-filter" alt="Run code" />
-                        <span>Run</span>
-                    </span>
-                    <span className="button" onClick={this.onCodeSave.bind(this)}>
-                        <img src="./assets/saveicon.svg" width="20" className="purple-filter" alt="Save code" />
-                        <span className="push-right">Save</span>
-                    </span>
-                </div>
+            <div className="App">
+                <label htmlFor="mode">Select mode: </label>
+                <select id="mode" name="mode" onChange={this.setMode.bind(this)}>
+                    <option value="eval">Eval</option>
+                    <option value="types">Infer types</option>
+                    <option value="bytecode">Bytecode</option>
+                    <option value="js">Desugar JS</option>
+                </select>
+                &nbsp;
+                <input type="checkbox" name="autoeval" onChange={this.toggleAutorun.bind(this)} defaultChecked></input>
+                <label htmlFor="autoeval">Autorun</label>
+                &nbsp;
+                <button onClick={this.onCodeRun.bind(this)}>Run</button>
 
-                <div id="editor">
-                    <Suspense fallback={Empty}>
-                        <Editor
-                            language="javascript"
-                            theme="vs-dark"
-                            options={{
-                                fontSize: 17
-                            }}
-                            onMount={this.editorDidMount.bind(this)}
-                        />
-                    </Suspense>
-                </div>
 
-                <div id="console"></div>
-
-                <footer>
-                    Made with ❤️ by y21
-                    <a href="https://github.com/y21/dash" rel="noreferrer" target="_blank" id="view-source">
-                        <img src="./assets/github.svg" width="20" id="github-icon" alt="GitHub" />
-                        View source
-                    </a>
-                </footer>
+                <Suspense fallback={Empty}>
+                    <Editor
+                        language="javascript"
+                        theme="vs-light"
+                        width={700}
+                        height={250}
+                        options={{
+                            fontSize: 13
+                        }}
+                        onMount={this.editorDidMount.bind(this)}
+                        onChange={this.onType.bind(this)}
+                    />
+                </Suspense>
+                <h3>Output</h3>
+                <textarea cols={75} rows={20} className="editor" value={this.state.output}>
+                </textarea>
             </div >
         )
     }
